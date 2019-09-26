283aae8e64cdab887d0f8b3cfa236f7c6ec01f9c
Chromicle <ajayprabhakar369@gmail.com>
2019-09-26 13:17:43 +0530

    Java Script vriables

---
+++ bolt/wbaisics/javascript.js
@@ -0,0 +1,12 @@
+var c = 3 + 4; // Addition operation on numbers.
+    console.log ("The value of c is ", c);
+    var x = 2;
+    var y = 3;
+    var z = x - y; // Subtraction operation on variables.
+    console.log ("The value of z is ", z);
+    var m = x * y; // Multiplication operation on variables.
+    console.log ("The value of m is ", m);
+    var n = x / y; // Division operation on variables.
+    console.log ("The value of n is ", n);
+    var w = (x * y + 20) - 10 // Arithmetic operation on an expression.
+    console.log ("The value of w is ", w);
