63c3bf52f49f19172136059982a3476109cd6fab
Chromicle <ajayprabhakar369@gmail.com>
2019-10-01 22:29:01 +0530

    Google visualation library graphs

---
+++ bolt/plantMonitor/plant_monitor.js
@@ -0,0 +1,12 @@
+setChartLibrary('google-chart');
+setChartTitle('Plant Monitor');
+setChartType('areaGraph');
+//lineGraph
+//scatterGraph
+//barGraph
+//tableGraph
+//steppedGraph
+//HistogramGraph
+setAnimation('true');
+setAxisName('Time','light sensivity');
+plotChart("time_stamp","light");
