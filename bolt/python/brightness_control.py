d67d01dba391a6babeb7116d4671d2bed471ff38
Chromicle <ajayprabhakar369@gmail.com>
2019-10-04 21:51:18 +0530

    Controlled voltage

---
+++ bolt/python/brightness_control.py
@@ -0,0 +1,12 @@
+from boltiot import Bolt
+api_key = "b1bba2fd-6b2e-42e6-98e0-ddfb295736f8"
+device_id  = "BOLT5647738"
+mybolt = Bolt(api_key, device_id)
+inp =input()
+if(inp=='ON' or inp=='on'):
+    response = mybolt.digitalWrite('0','HIGH')
+elif(inp=='OFF' or inp=='off'):
+    response = mybolt.digitalWrite('0','LOW')
+else:
+    response=mybolt.analogWrite('0',inp)
+print (response)
